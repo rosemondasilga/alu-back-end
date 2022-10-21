f394d65915eb933a48188cc6dfde622722472f77
rosemondasilga <rosemondasilga@gmail.com>
2022-10-21 19:56:48 +0000

    hey

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-""" export data from csv """
+"""import to csv"""
 
-import csv
-import json
 import requests
 import sys
-if __name__ == "__main__":
-    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-    res = requests.get(link)
-    user = json.loads(res.text)
-    num = sys.argv[1]
-    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
-    res = requests.get(link)
-    todos = json.loads(res.text)
-    csv_data = [["{}".format(i["userId"]),
-                user["username"],
-                "{}".format(i["completed"]),
-                 i["title"]] for i in todos]
-    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-        writer.writerows(csv_data)
 
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+
+    employee_name = user_info["name"]
+    employee_username = user_info["username"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+
+    with open(str(employee_id) + '.csv', "w") as file:
+        [file.write('"' + str(employee_id) + '",' +
+                    '"' + employee_username + '",' +
+                    '"' + str(task["completed"]) + '",' +
+                    '"' + task["title"] + '",' + "\n")
+         for task in todos_info]
